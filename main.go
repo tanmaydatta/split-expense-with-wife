3d23022ca33cd7252e0e35abfde2490518706f58
Tanmay Datta <tanmaydatta@gmail.com>
2023-03-01 14:28:37 +0000

    Move code to function fir

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"fmt"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World!",
-	}, nil
-}
-
 func main() {
-	lambda.Start(handler)
+	fmt.Println("Hello")
 }
