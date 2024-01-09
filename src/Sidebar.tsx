539e0c5b7b4b2b264f175265cdf1dd76b742ffef
Tanmay <tanmaydatta@monzo.com>
2024-01-09 10:22:35 +0000

    refactor

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,10 +1,24 @@
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 
 function Sidebar(): JSX.Element {
   const navigate = useNavigate();
+  const data = useSelector((state: any) => state.value);
   return (
     <div className="Sidebar">
+      <div className="SidebarHeader">
+        {data.users && (
+          <div>
+            Welcome{" "}
+            {
+              data.users?.find(
+                (u: { Id: number; FirstName: string }) => u.Id === data.userId
+              )?.FirstName
+            }
+          </div>
+        )}
+      </div>
       <div className="SidebarItem" onClick={() => navigate("/")}>
         Add
       </div>
@@ -17,8 +31,8 @@ function Sidebar(): JSX.Element {
       <div className="SidebarItem" onClick={() => navigate("/budget")}>
         Budget
       </div>
-      <div className="SidebarItem" onClick={() => navigate("/logout")}>
-        Logout
+      <div className="SidebarFooter" onClick={() => navigate("/logout")}>
+        <div>Logout</div>
       </div>
     </div>
   );
