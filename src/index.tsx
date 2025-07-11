22e9e7aacda31eeeae6644c8c9237e15d6519e7c
Tanmay Datta <tanmay.datta@preply.com>
2025-07-11 17:48:12 +0100

    pwa try

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import AppWrapper from "./AppWrapper";
 import "./index.css";
 import { persistor, store } from "./redux/store";
-
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -25,7 +24,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
