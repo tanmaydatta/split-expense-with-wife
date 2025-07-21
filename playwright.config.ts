70094994d574549092e270dbddcc996ad6acb3f3
Tanmay Datta <tanmay.datta@preply.com>
2025-07-21 17:15:22 +0100

    refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? [['list'], ['html']] : 'html',
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
